fccae30100576cc56f022752025a763b56afa367
scotch02 <sergey.utupin@gmail.com>
2014-12-29 17:18:35 +0200

    sql

---
+++ data_customer.sql
@@ -0,0 +1,6 @@
+﻿INSERT INTO customer (id, name, password) VALUES
+	(1, 'Serious Sam', 'password'),
+	(2, 'Wild Bill', 'password'),
+	(3, 'Dieter Bohlen', 'password'),
+	(4, 'Tom Cruise', 'password'),
+	(5, 'Vasya', 'password');
---
+++ data_indent.sql
@@ -0,0 +1,6 @@
+﻿INSERT INTO indent (id, castomer_id, description, status) VALUES
+	(1, 1, 'Some descripion of this order', 'new'), --'new', 'inprogress', 'done'
+	(2, 1, 'Some descripion of this order', 'inprogress'),
+	(3, 3, 'Some descripion of this order', 'done'),
+	(4, 4, 'Some descripion of this order', 'new'),
+	(5, 4, 'Some descripion of this order', 'inprogress');
