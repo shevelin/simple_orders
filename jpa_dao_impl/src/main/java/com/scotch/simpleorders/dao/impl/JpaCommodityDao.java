30471174e7026b8e85f2e9c31a2d891488b927f3
scotch02 <sergey.utupin@gmail.com>
2015-01-04 21:52:17 +0200

    04.01.2015

--- jpa_dao_impl/src/main/java/com/scotch/simpleorders/dao/impl/JpaCommodityDao.java
+++ jpa_dao_impl/src/main/java/com/scotch/simpleorders/dao/impl/JpaCommodityDao.java
@@ -1,6 +1,7 @@
 package com.scotch.simpleorders.dao.impl;
 
 import com.scotch.simpleorders.dao.CommodityDao;
+import com.scotch.simpleorders.entity.Category;
 import com.scotch.simpleorders.entity.Commodity;
 
 import java.util.List;
@@ -9,5 +10,23 @@ import java.util.List;
  * Created by sutupin on 30.12.2014.
  */
 public class JpaCommodityDao extends AbstractJpaDao<Commodity, Integer> implements CommodityDao {
+    @Override
+    protected Commodity getByUnique(Commodity entity) {
+        return null;
+    }
 
+    @Override
+    protected String getAllQueryString() {
+        return null;
+    }
+
+    @Override
+    public void removeAllForCategory(Category category) {
+
+    }
+
+    @Override
+    public List<Commodity> getAllForCategory(Category category) {
+        return null;
+    }
 }
